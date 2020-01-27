3aeeb38d4162a8a95d39dcc8fbf4582525764e71
admin <lakshmi.patil.nine@gmail.com>
2020-01-27 17:38:54 +0530

    added: calculated gambler won or lost 50% of stake

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,15 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "Welcome to the Gambling Simulation"
 STAKE=100
 BET=1
 cash=$STAKE
 
-if (( $((RANDOM%2))==1 ))
-then
-	cash=$((cash+BET))
-	echo win
-else
-	cash=$((cash-BET))
-	echo loose
-fi
+gamble()
+{
+percentage
+while(( cash > MIN_STAKE && cash < MAX_STAKE ))
+do
+	if (( $((RANDOM%2))==1 ))
+	then
+		cash=$((cash+BET))
+		echo -e "Win \n cash= $cash"
+	else
+		cash=$((cash-BET))
+		echo -e "Loose \n cash= $cash"
+	fi
+done
+}
+
+percentage()
+{
+	MIN_STAKE=$(( STAKE - $((STAKE*50/100)) ))
+	MAX_STAKE=$(( STAKE + $((STAKE*50/100)) ))
+}
+gamble

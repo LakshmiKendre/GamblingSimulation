5a3869d5df9351c46c60c6bf76616a9eb57b24a2
admin <lakshmi.patil.nine@gmail.com>
2020-01-27 15:25:07 +0530

    added: checking gambler is win or loose by making bet

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -3,4 +3,13 @@
 echo "Welcome to the Gambling Simulation"
 STAKE=100
 BET=1
+cash=$STAKE
 
+if (( $((RANDOM%2))==1 ))
+then
+	cash=$((cash+BET))
+	echo win
+else
+	cash=$((cash-BET))
+	echo loose
+fi
